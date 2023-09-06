e87377ca64570fa0356779d8f46401beb0b52803
Eyad muhammad <eyadmuhammad90@gmail.com>
2023-09-06 17:36:03 +0300

    news v-1.0

---
+++ src/index.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal("header .container", {
+  origin: "top",
+  duration: 500,
+});
+ScrollReveal().reveal("#main-bg", {
+  delay: 200,
+  origin: "left",
+  duration: 1000,
+  distance: `300px`,
+});
+ScrollReveal().reveal("#news", {
+  delay: 500,
+  origin: "right",
+  duration: 1000,
+  distance: `300px`,
+});
+ScrollReveal().reveal("article", {
+  delay: 500,
+  origin: "bottom",
+  duration: 1000,
+  distance: `300px`,
+});
+ScrollReveal().reveal("footer .card", {
+  delay: 500,
+  origin: "top",
+  interval: 300,
+  duration: 1000,
+});
